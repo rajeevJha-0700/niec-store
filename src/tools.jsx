3ead66a71d0e287333d4383fa820eb5fe2d52c39
rajeevJha-0700 <rajeevranjanjha0001@gmail.com>
2025-10-31 02:14:50 +0530

    added new pages

--- src/tools.jsx
+++ src/tools.jsx
@@ -8,5 +8,11 @@ import Product from './Component/Product.jsx'
 import EditProduct from './Component/EditProduct.jsx'
 import Cart from './Component/Cart.jsx'
 import SellerInfo from './Component/SellerInfo.jsx'
-
-export {Protected,Home,Login,Signup,MyStore,Product,PushProduct,EditProduct,Cart,SellerInfo};
+import PageNotFound from './Component/PageNotFound.jsx'
+import About from './Component/About.jsx'
+import TermsOfServices from './Component/TermsOfServices.jsx'
+import ComingSoon from './Component/ComingSoon.jsx'
+import PrivacyPolicy from './Component/PrivacyPolicy.jsx'
+import FAQ from './Component/FAQ.jsx'
+import Contact from './Component/Contacts.jsx'
+export {Protected,Home,Login,Signup,MyStore,Product,PushProduct,EditProduct,Cart,SellerInfo,PageNotFound,About,TermsOfServices,PrivacyPolicy,ComingSoon,FAQ,Contact};
